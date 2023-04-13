96b69354658e9a6835421e99f6910e7e26c3b8dd
Leonardo <leoinsalata@gmail.com>
2023-04-12 21:12:27 -0300

    java intro

---
+++ JavaIntro03/src/javaintro03/JavaIntro03.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaintro03;
+
+/**
+ *
+ * @author Leo
+ */
+public class JavaIntro03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+System.out.println(hola)
+int num1 = 5;
+int num2 = 5;
+int suma = num1 + num2;
+double division = num1 / num2;
+boolean logico = num2 < num1;
+num1++;
+   
+}
